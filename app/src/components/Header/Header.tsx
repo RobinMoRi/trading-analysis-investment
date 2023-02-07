f51096a1f574331dc54e872c149611cca6968fbf
RobinMoRi <robin.moreno.rinding@gmail.com>
2023-02-07 21:57:39 +0100

    added spa

---
+++ app/src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Typography, Box, useTheme } from '@mui/material';
+import { tokens } from '../../theme';
+
+const Header = ({title, subtitle}: {title: string, subtitle: string}) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box ml="20px" mb="30px">
+            <Typography 
+                variant="h2" 
+                color={colors.grey[100]} 
+                fontWeight="bold" 
+                sx={{ mb: "5px" }}>{title}
+            </Typography>
+            <Typography
+                variant="h5" 
+                color={colors.greenAccent[400]} 
+                fontWeight="bold" 
+                sx={{ mb: "5px" }}>{subtitle}
+            </Typography>
+        </Box>
+    );
+}
+
+export default Header;
