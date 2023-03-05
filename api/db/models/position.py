c40b85360e51725b63e2b09b885e72f1532e8220
RobinMoRi <robin.moreno.rinding@gmail.com>
2023-03-05 11:44:48 +0100

    added position model

--- api/db/models/position.py
+++ api/db/models/position.py
@@ -4,29 +4,22 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum, DateTim
 from sqlalchemy.orm import relationship
 from db.models.company import Company
 from sqlalchemy.sql import func
-
+from db.models.netassetvalue import ValueType
 from db.database import Base
 
-class AssetType(enum.Enum):
-    rebate = 0
-    premium = 1
-
-class ValueType(enum.Enum):
-    reported = 0
-    computed = 1
-
-class NetAssetValue(Base):
-    __tablename__ = "net_asset_values"
+class Position(Base):
+    __tablename__ = "positions"
 
     id = Column(Integer, primary_key=True, index=True)
     val = Column(Float, unique=False, index=True)
-    ticker = Column(String, unique=False, index=True)
-    asset_type = Column(Enum(AssetType), unique=False)
+    weight = Column(Float, unique=False, index=True)
+    position = Column(Float, unique=False, index=True)
+    buy = Column(Float, unique=False, index=True)
     value_type = Column(Enum(ValueType), unique=False)
     company_id = Column(Integer, ForeignKey("companies.id"), nullable=False)
-    position_id = Column(Integer, ForeignKey("positions.id"), nullable=False)
+
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-    company = relationship("Company", back_populates="netassetvalues")
-    position = relationship("Position", back_populates="netassetvalue")
+    company = relationship("Company", back_populates="positions")
+    netassetvalue = relationship("NetAssetValue", back_populates="position")
--- api/schemas/company.py
+++ api/schemas/company.py
@@ -1,12 +1,12 @@
 from typing import List, Union
-
+from typing import Optional
 from pydantic import BaseModel
 
 class CompanyBase(BaseModel):
     ticker: str
     yf_ticker: str
     name: str
-    price: float
+    price: Optional[float] = None
 
 class Company(CompanyBase):
     id: int
